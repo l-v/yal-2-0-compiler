06d3b169805691fa17b4cfc3cffa2040b6ba6baa
lv._@hotmail.com <lv._@hotmail.com@4ff9513f-6237-28b9-d0fa-8b5a1b2d8a5b>
2011-05-06 21:54:09 +0000

    Tabela de simbolos já armazena informacao sobre declaracoes globais, nomes funcoes e respectivos argumentos.

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -7,99 +7,238 @@ public class SymbolTable extends Object {
 
 
 	  String type;  //tipo funcao
-	   String value; //nome funcao
-	   LinkedList<SymbolTable> functions = new LinkedList<SymbolTable>();
-	   HashMap<String, String> localVars = new HashMap<String, String>(); // variaveis dento de modulo/funcao
+	  String name; //nome funcao
+	  LinkedList<SymbolTable> childTables;
+	  SymbolTable parentTable;
+	  LinkedList<Variable> localVars;
 
+	  Boolean isFunc; //verdadeiro se esta tabela de simbolos esta associada a uma funcao
+	  LinkedList<Variable> funcArgs; //argumentos da funcao
+	 
+ 
+	  /*** Construtores ***/
 	  public SymbolTable() {
-	    functions = new LinkedList<SymbolTable>();
-	    localVars = new HashMap<String, String>();
+	    childTables = new LinkedList<SymbolTable>();
+	    localVars = new LinkedList<Variable>();
 
-	  }
-	  
-	  public SymbolTable(String itype, String ivalue) {
-	    
-	    type=itype;
-	    value=ivalue;
-	    
-	    functions = new LinkedList<SymbolTable>();
-	    localVars = new HashMap<String, String>();
-
-	  }
-	  
-	  public  void defineMainVars(String itype, String ivalue) {
-	    type=itype;
-	    value=ivalue;
-	  }
-	  
-	  public void addEntry(String type, String value) {
-	    localVars.put(type, value);
-	  }
-	  
-	  public void addFunc(SymbolTable newFunc) {
-	    functions.add(newFunc);
-	  }
-	  
-	  public void addFunc(String type, String value) {
-	    SymbolTable newFunc = new SymbolTable(type, value);
-	    functions.add(newFunc);
-	  }
-	  
-	  public int addToEnd(SymbolTable newFunc) {
-	    
-	    if (functions.size()==0)
-	      return -1;
-
-	    functions.getLast().addFunc(newFunc);
-	    return 1;
-	  }
-	 
-	 
-	 /* Procura na LinkedList functions por uma funcao de nome/valor 'value' */ 
-	  public int getListIndex(String funcValue) {
-	    for(int i=0; i!=functions.size(); i++) {
-	      if (funcValue.equals(functions.get(i).value))
-		return i;
-	    
-	    }
-	    
-	    return -1;
-	  }
-	 
-	 /* Insere uma nova funcao (newFunc) dentro de uma funcao especifica (parentFunc) */
-	  public int insertIntoFunc(SymbolTable parentFunc, SymbolTable newFunc) {
-	    return 0;
-	  }
-	  
-	  public void printST() {
-	      System.out.println("\n--SymbolTable--\n");
-	      this.printST("");
+	
+	    isFunc = false;
+	    funcArgs = new LinkedList<Variable>();
+	    parentTable = null;
 	  }
 
-	  // print local vars
-	  public void printST(String spacing) {
+	  public SymbolTable(String sType, SymbolTable parent) {
+	    childTables = new LinkedList<SymbolTable>();
+	    localVars = new LinkedList<Variable>();
+	    type = sType;
 
-	    System.out.println(spacing + type + " : " + value 
-				+ "\n" + spacing + "-localVars: " + localVars.size()
-				+ ";\t childFuncs: " + functions.size());
+	
+	    isFunc = false;
+	    funcArgs = new LinkedList<Variable>();
+	    parentTable = null;
+	  }
+
+	  public SymbolTable(Node root) {
+	    childTables = new LinkedList<SymbolTable>();
+	    localVars = new LinkedList<Variable>();
+	
+	    isFunc = false;
+	    funcArgs = new LinkedList<Variable>();
+	    parentTable = null;
 	    
-	    Iterator iterator = localVars.keySet().iterator();  
-	    while (iterator.hasNext()) {  
-		String key = iterator.next().toString();  
-		String value = localVars.get(key).toString();  
+
+	    addTree(root);
+
+	  }
+
+	  public void addTree(Node root) {
+
+	    int childNum = root.jjtGetNumChildren();
+	    for (int i=0; i!=childNum; i++) {
+		
+		Node newNode = root.jjtGetChild(i);
+		System.out.println("--> "+newNode.toString() + ":" + newNode.getVal());
+
+		String varName = newNode.getVal();
+		String varType = newNode.toString();
+
+
+		if (varType.equals("MID"))
+		    editTable(varName, varType);
+
+		else if (varType.equals("Decl")) 
+		    addDeclNode(newNode);
       
-		System.out.println(spacing + "   " + key + " " + value);  
-	    }  
+		else if (varType.equals("Func"))
+		    addFuncNode(newNode);
+		
+	    }
+	  }
 
+	  /*** Adiciona um nó contendo uma declaracao de variavel ***/
+	  public void addDeclNode(Node node) {
+		
+		Node DeclID = node.jjtGetChild(0);
+		System.out.println("--> "+DeclID.toString() + ":" + DeclID.getVal());
+		
+
+		for (int i=1; i!=node.jjtGetNumChildren(); i++) {
+		    
+		   if (node.jjtGetChild(i).toString().equals("ArraySize")) { 
+		      addVar(DeclID.getVal(), "int[]");
+		      return;
+		   }
+		}
+
+		addVar(DeclID.getVal(), "int");
+	  }
+
+	  /*** Adiciona um nó de funcao ***/
+	  public void addFuncNode(Node node) {
+
+	     SymbolTable newTable = new SymbolTable("Func", this); 
+	     newTable.setFunc(1);
+
+		
+	     Node FuncID = node.jjtGetChild(0);
+	     System.out.println("--> "+FuncID.toString() + ":" + FuncID.getVal());
+
+	     newTable.editTable(FuncID.getVal(), node.toString());
+	    
+
+	     // adiciona argumentos da funcao, se estes existirem
+	     if (node.jjtGetNumChildren()>2) {  
+		  
+		  int index=1;
+		  for (int i=1; i!=node.jjtGetNumChildren(); i++) {
+		      if (node.jjtGetChild(i).toString().equals("Args"))  {
+			  index = i;
+			  break;
+		      }
+		  }
+
+		  Node Args = node.jjtGetChild(index);
+		 
+
+		  int numArgs = Args.jjtGetNumChildren();
+		  for (int i=0; i!=numArgs; i++) {
+		      Node newArg = Args.jjtGetChild(i);
+
+		      // verifica se arg é do tipo array (MELHORAR METODO...)
+		      if (i<numArgs-1 && Args.jjtGetChild(i+1).toString().equals("IsArray")) {
+			newTable.addArg(newArg.getVal(), "int[]");
+			i++;
+		      }
+
+		      else {
+			newTable.addArg(newArg.getVal(), "int");
+		      }
+		  }
+	     }
+  
+	     addTable(newTable);
+
+	  }
+
+	  /*** Edita atributos type e name ***/
+	  public void editTable(String sName, String sType) {
+	    type = sType;
+	    name = sName;
+	  }
+
+	  public void setFunc(int isFunction) {
+	    
+	    if (isFunction == 1)
+		isFunc = true;
+	    else
+		isFunc = false;
+	  }
+
+	  /*** Adiciona nova variavel a localVars ***/
+	  public void addVar(String vName, String vType) {
+	    Variable newVar = new Variable(vName, vType);
+	    localVars.add(newVar);
+	  }
+
+	  /*** Adiciona nova tabela de simbolos a childTables***/ 
+	  public void addTable(SymbolTable table) {
+	     childTables.add(table);
+
+	  }
+
+	  /*** Adiciona novo argumento de funcao ***/
+	  public void addArg(String vName, String vType) {
+	     Variable newVar = new Variable(vName, vType);
+	     funcArgs.add(newVar);
+	  }
+
+
+
+	
+
+	  public void printTable() {
+	      System.out.println("\n--SymbolTable--\n");
+	      this.printTable("");
+	  }
+
+	  // POR MAIS LEGIVEL!
+	  public void printTable(String spacing) {
+
+	    System.out.println(spacing + type + " : " + name 
+				+ "\n" + spacing + "-localVars: " + localVars.size()
+				+ ";\t childTables: " + childTables.size());
+	    
+	    // imprime argumentos funcao 
+	    
+	    if (isFunc && funcArgs.size()>0) {
+		System.out.println("__FuncArgs:__");
+		for (int i=0; i!=funcArgs.size(); i++) {
+		    funcArgs.get(i).printVar();
+		}
+		System.out.println("_____________");
+	    }
+	   
+
+
+	    // imprime variaveis locais
+	    System.out.println("--localVars:--");
+	    for (int i=0; i!=localVars.size(); i++) {
+		Variable var = localVars.get(i);
+		System.out.println("\n"+spacing); 
+		var.printVar();
+		 
+	    }  
+	    System.out.println("--------------");
 
 	    // print 'child' functions
-	  
 	    String space = spacing + "   ";
-	    
-	    for (int i=0; i!=functions.size(); i++) {
-		functions.get(i).printST(space);
+	    for (int i=0; i!=childTables.size(); i++) {
+		childTables.get(i).printTable(space);
 	    }
 	  
 	  }
 
 	}
+
+class Variable {
+      
+	String type;
+	String name;
+
+	public Variable(String vName, String vType) {
+	    type = vType;
+	    name = vName;
+	}
+
+	public String getAttribute(String attribute) {
+	    if (attribute.equalsIgnoreCase("type"))
+		return type;
+	    else
+		return name;
+	}
+
+	public void printVar() {
+	    System.out.println(name + ":" + type);
+	}
+	
+}
