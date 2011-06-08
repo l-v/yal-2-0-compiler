ed95904d9825ee67fce358606ac5caebf30b9f51
andre.anpa.xd@gmail.com <andre.anpa.xd@gmail.com@4ff9513f-6237-28b9-d0fa-8b5a1b2d8a5b>
2011-06-08 11:35:18 +0000

    Generação de código até às funções.

--- trunk/src/CodeGenerator.java
+++ trunk/src/CodeGenerator.java
@@ -1,97 +1,262 @@
 import java.io.*;
-
+import java.util.*;
 
 public class CodeGenerator extends Object {
 
   SymbolTable st;
   String filename;
+  HashMap<Variable,Integer> variables;
+  int regCounter;
 
-  CodeGenerator(String file, SymbolTable codeSt) {
+  
+  CodeGenerator(String file, SymbolTable codeSt, Node rootNode) {
       
       st = codeSt;
       filename = file;
-
-
-      String initialization = ".class public " + st.name;
-      initialization += "\n.super java/lang/Object";
-
-
-
-      // get declarations
-      int declNum = st.localVars.size();
-      String declarations = "";//".field static ";
-      for (int i=0; i!=declNum; i++) {
-	  Variable decl = st.localVars.get(i);
-
-	  String varName = decl.getAttribute("name");
-	  String varSize = decl.getAttribute("arraySize");
-
-	  declarations += "\n";
-
-	  if (!varSize.equals("-1")) { // declaracao do tipo a=[100]
-		
-
-		String declStr = "\n.field static " + varName + " [I";
-		declStr += "\n\n; static code goes here\n ; we need this to create the array " + varName + " (with " + varSize + " elements)";
-			   
-		declStr += "\n.method static public <clinit>()V";
-		declStr += "\n.limit stack 2";
-		declStr += "\n.limit locals 0";
-
-		declStr += "\n\n; " + varName + " = new int[" + varSize + "];";
-		declStr += "\nbipush " + varSize;
-		declStr += "\nnewarray int";
-		declStr += "\nputstatic " + st.name + "/" + varName + " [I";
-
-		declStr += "\nreturn";
-		declStr += "\n.end method";
-
-		declarations += declStr;
+      variables = new HashMap<Variable, Integer>();
+      regCounter = 0;
+      
+      String result = "";
+      String header = "";
+      String globals = "";
+      String arrayInitializations = "";
+      String functions = "";
+	  
+	  int childNum = rootNode.jjtGetNumChildren();
+	  
+	  for(int i = 0; i < childNum ; i++)
+	  {
+		  Node newNode = rootNode.jjtGetChild(i);
+		  
+		  String varName = newNode.getVal();
+		  String varType = newNode.toString();
+		  
+			if (varType.equals("MID"))
+			{
+				header = ".class public " + varName + "\n";
+			    header += ".super java/lang/Object\n";
+			}
+	
+			else if (varType.equals("Decl"))
+			{
+				globals += declareGlobal(newNode);
+				arrayInitializations += initializeArrays(newNode);
+			}
+	      
+			else if (varType.equals("Func"))
+			    functions += declareFunction(newNode);
 	  }
 
-      }
-
-
-      String contents = initialization + declarations;
-      createFile(contents);
-
-      /*
-
-      int depth = st.childTables.size(); 
-      for (int i=0; i!=depth; i++) {
-	  SymbolTable child = st.get(i);
-	  
-
-	 // if ()
-	  
-
-      }
-*/
-
+      result += header + "\n" + globals + "\n" + arrayInitializations + "\n" + functions;
+      
+      System.out.println(result);
+      
+      createFile(result);
 
   }
 
+  public String declareGlobal(Node declNode)
+  {
+	  String result = "";
+	  
+	  int childNum = declNode.jjtGetNumChildren();
+	  
+	  for(int i = 0; i < childNum ; i++)
+	  {
+		  Node newNode = declNode.jjtGetChild(i);
+		  
+		  if(newNode.toString().equals("DeclID"))
+		  {
+			  String name = newNode.getVal();
+			  result += ".field static ";
+			  result += name + " ";
+			  
+			  if(i == childNum-1)
+				  result += "I\n";
+		  }
+		  else if(newNode.toString().equals("AddSubOP"))
+		  {
+			  String signal = newNode.getVal();
+			  i++;
+			  String value = declNode.jjtGetChild(i).getVal();
+			  
+			  result += "I " + signal + value + "\n";
+		  }
+		  else if(newNode.toString().equals("InicVar"))
+		  {
+			  String value = newNode.getVal();
+			  result += "I " + value + "\n";
+		  }
+		  else if(newNode.toString().equals("ArraySize") ||
+				  newNode.toString().equals("IsArray"))
+			  result += "[I\n";		  
+	  }
+	  
+	  return result;
+  }
+  
+  public String initializeArrays(Node declNode)
+  {
+	  String result = "";
+	  
+	  int childNum = declNode.jjtGetNumChildren();
+	  
+	  for(int i = 0; i < childNum ; i++)
+	  {
+		  Node newNode = declNode.jjtGetChild(i);
+		  
+		  if(newNode.toString().equals("ArraySize"))
+		  {			  
+			  String name = declNode.jjtGetChild(0).getVal();
+			  String size = newNode.jjtGetChild(0).getVal();
+			  
+			  result += ".method static public <clinit>()V\n";
+			  result += " .limit stack 2\n";
+			  result += " .limit locals 0\n";
+			  
+			  result += " bipush " + size + "\n";
+			  result += " newarray int\n";
+			  result += " putstatic " + st.name + "/" + name  + " [I\n";
 
-
-
-
-
-
+			  result += " return\n";
+			  result += ".end method\n\n";
+		  }  
+	  }
+	  return result;
+  }
+  
+  public String declareFunction(Node funcNode)
+  {
+	  String result = "";
+	  String header = ".method public static";
+	  String name = "";
+	  String ret = "V";
+	  String args = "";
+	  String body = "";
+	  String footer = ".end method";
+	  
+	  int childNum = funcNode.jjtGetNumChildren();
+	  
+	  for(int i = 0; i < childNum; i++)
+	  {
+		  Node newNode = funcNode.jjtGetChild(i);
+		  
+		  if(newNode.toString().equals("FuncID"))
+		  {
+			  if(funcNode.jjtGetChild(i+1).toString().equals("IsArray"))
+				  ret = "[I";
+			  else if(funcNode.jjtGetChild(i+1).toString().equals("AssignID"))
+				  ret = "I";
+			  else
+				  name = newNode.getVal();
+		  }
+		  else if(newNode.toString().equals("AssignID"))
+			  name = newNode.getVal();
+		  else if(newNode.toString().equals("Args"))
+			  args += translateArgs(newNode);
+		  else if(newNode.toString().equals("FuncBody"))
+			  body += translateFuncBody(newNode,name);
+	  }
+	
+	  if(name.equals("main"))
+		  args = "[Ljava/lang/String;";
+	  
+	  result += header + "\n";
+	  result += name;
+	  result += "(" + args + ")";
+	  result += ret + "\n";
+	  result += body + "\n";
+	  result += footer + "\n\n";
+	  
+	  return result;
+  }
+  
+  public String translateArgs(Node argsNode)
+  {
+	  String result = "";
+	  
+	  int argsNum = argsNode.jjtGetNumChildren();
+	  
+	  for(int i = 0; i < argsNum; i++)
+	  {
+		  Node newNode = argsNode.jjtGetChild(i);
+		  
+		  if(newNode.toString().equals("VarListID"))
+		  {
+			  result += "I";
+			  
+			  if(i != argsNum-1 && argsNode.jjtGetChild(i+1).toString().equals("IsArray"))
+			  {
+				  result = "[I";
+				  i++;
+			  }
+		  }
+	  }
+	  
+	  return result;
+  }
+  
+  public String translateFuncBody(Node bodyNode, String funcName)
+  {
+	  String result = "";
+	  
+	  SymbolTable functionST = st.getSymbolTable("Func", funcName); //Tabela de simbolos correspondente a funcao
+	  
+	  String limitStack = ".limit stack ";
+	  String limitLocals = ".limit locals ";
+	  
+	  int numArgs = functionST.funcArgs.size();
+	  int numLocal= functionST.localVars.size();
+	  int numStack = 0;
+	  
+	  limitLocals += (numArgs + numLocal);
+	  
+	  int numOfStmtLst = bodyNode.jjtGetNumChildren();
+	  
+	  for(int i = 0; i < numOfStmtLst; i++)
+	  {
+		  Node newNode = bodyNode.jjtGetChild(i);
+		  
+		  if(newNode.toString().equals("Assign"))
+		  {
+			  Node left = newNode.jjtGetChild(0);
+			  Node right = newNode.jjtGetChild(1);
+		  }
+		  else if(newNode.toString().equals("While"))
+		  {
+			  
+		  }
+		  else if(newNode.toString().equals("If"))
+		  {
+			  
+		  }
+		  else if(newNode.toString().equals("Call"))
+		  {
+			  
+		  }
+	  }
+	    
+	  result += limitStack + numStack + "\n";
+	  result += limitLocals + "\n";
+	  
+	  return result;
+  }
+  
   public void createFile(String contents) {
       try{
 	// Create file 
-	FileWriter fstream = new FileWriter("out.j");
+	
+    FileWriter fstream = new FileWriter("out.j");
 	BufferedWriter out = new BufferedWriter(fstream);
 	out.write(contents);
 	//Close the output stream
 	out.close();
-	}catch (Exception e){//Catch exception if any
+	
+	System.out.println("File writed!");
+	
+    }catch (Exception e){//Catch exception if any
 	 System.err.println("Error: " + e.getMessage());
 	}
   }
-
-
-
-
 
 }
--- trunk/src/SymbolTable.java
+++ trunk/src/SymbolTable.java
@@ -9,7 +9,6 @@ import java.util.*;
 ***/
 public class SymbolTable extends Object {
 
-
 	  String type;  //tipo funcao
 	  String name; //nome funcao
 
@@ -21,91 +20,81 @@ public class SymbolTable extends Object {
 	  LinkedList<Variable> funcArgs; //argumentos da funcao
 	  String returnType;
 
-	
 	  
-	 
- 
-	  /*** Construtores ***/
+	  //Construtores
 	  public SymbolTable() {
-	    childTables = new LinkedList<SymbolTable>();
+		childTables = new LinkedList<SymbolTable>();
 	    localVars = new LinkedList<Variable>();
-
-	
-	    isFunc = false;
 	    funcArgs = new LinkedList<Variable>();
+	    isFunc = false;
 	    parentTable = null;
 	  }
 
 	  public SymbolTable(String sType, SymbolTable parent) {
 	    childTables = new LinkedList<SymbolTable>();
 	    localVars = new LinkedList<Variable>();
+	    parentTable = parent;
 	    type = sType;
-
-	
 	    isFunc = false;
 	    funcArgs = new LinkedList<Variable>();
-	    parentTable = parent;
 	  }
 
 	  public SymbolTable(Node root) {
 	    childTables = new LinkedList<SymbolTable>();
 	    localVars = new LinkedList<Variable>();
-	
-	    isFunc = false;
 	    funcArgs = new LinkedList<Variable>();
+	    isFunc = false;
 	    parentTable = null;
 	    
-
 	    addTree(root);
 	  }
 
 	  public void addTree(Node root) {
 
 	    int childNum = root.jjtGetNumChildren();
+	    
 	    for (int i=0; i!=childNum; i++) {
 		
-		Node newNode = root.jjtGetChild(i);
-		//System.out.println("--> "+newNode.toString() + ":" + newNode.getVal());
-
-		String varName = newNode.getVal();
-		String varType = newNode.toString();
-
-
-		if (varType.equals("MID"))
-		    editTable(varName, varType);
-
-		else if (varType.equals("Decl")) 
-		    addDeclNode(newNode);
-      
-		else if (varType.equals("Func"))
-		    addFuncNode(newNode);
+			Node newNode = root.jjtGetChild(i);
+			//System.out.println("--> "+newNode.toString() + ":" + newNode.getVal());
+	
+			String varName = newNode.getVal();
+			String varType = newNode.toString();
+	
+			if (varType.equals("MID"))
+			    editTable(varName, varType);
+	
+			else if (varType.equals("Decl")) 
+			    addDeclNode(newNode);
+	      
+			else if (varType.equals("Func"))
+			    addFuncNode(newNode);
 		
 	    }
 	  }
 
-	  /*** Adiciona um nó contendo uma declaracao de variavel ***/
+	  // Adiciona um no contendo uma declaracao de variavel
 	  public void addDeclNode(Node node) {
 		
 		Node DeclID = node.jjtGetChild(0);
 		//System.out.println("--> "+DeclID.toString() + ":" + DeclID.getVal());
 		
-
 		for (int i=1; i!=node.jjtGetNumChildren(); i++) {
 		    
 		   if (node.jjtGetChild(i).toString().equals("ArraySize")) { 
 
 		      Node arraySize = (node.jjtGetChild(i)).jjtGetChild(0);
-
-
 		      addVar(DeclID.getVal(), "int[]", arraySize.getVal());
 		      return;
 		   }
+		   
 		}
-
 		addVar(DeclID.getVal(), "int");
 	  }
+	  
 
-	  /*** Adiciona um nó de funcao ***/ // FALTA VER RETORNO DE FUNCAO
+	  // Adiciona um no de funcao 
+	  // TODO: FALTA VER RETORNO DE FUNCAO
 	  public void addFuncNode(Node node) {
 
 	     SymbolTable newTable = new SymbolTable("Func", this); 
@@ -178,7 +167,8 @@ public class SymbolTable extends Object {
 	  }
 
 
-	  /*** Processa corpo de uma funcao ***/ // FALTA VER EXPRESSOES DE TESTE
+	  // Processa corpo de uma funcao
+	  // TODO: FALTA VER EXPRESSOES DE TESTE
 	  public void addFuncBody(Node node, SymbolTable mainTable) {
 
 	     int numStmt = node.jjtGetNumChildren();
@@ -252,7 +242,7 @@ public class SymbolTable extends Object {
 	  }
 
 
-	  /*** Adiciona uma instrução de Assign ***/
+	  // Adiciona uma instrucao de Assign
 	  public void addAssign(Node node, SymbolTable table) {
 
 	     Node lhs = node.jjtGetChild(0);
@@ -316,7 +306,7 @@ public class SymbolTable extends Object {
 	      
 	  }
 
-	  /*** Edita atributos type e name ***/
+	  // Edita atributos type e name
 	  public void editTable(String sName, String sType) {
 	    type = sType;
 	    name = sName;
@@ -360,51 +350,79 @@ public class SymbolTable extends Object {
 
 
 
-	
-
 	  public void printTable() {
-	      System.out.println("\n--SymbolTable--\n");
+	      System.out.println("\n\n-----SymbolTable-----\n");
 	      this.printTable("");
 	      
 	  }
 
-	  // POR MAIS LEGIVEL!
+	  //TODO: POR MAIS LEGIVEL!
 	  public void printTable(String spacing) {
-
-	    System.out.println(spacing + type + " : " + name 
-				+ "\n" + spacing + "-localVars: " + localVars.size()
-				+ ";\t childTables: " + childTables.size());
+  
+	    System.out.println(spacing + type + " : " + name + "\n");
 	    
+	    // imprime variaveis locais
+	    System.out.println(spacing + "noLocalVars: " + localVars.size());
+	    
+	    for (int i=0; i!=localVars.size(); i++) {
+	    	Variable var = localVars.get(i);
+	    	System.out.print("\t" + spacing);
+	    	var.printVar();
+	    }
+	    System.out.println();
+	        
 	    // imprime retorno e argumentos funcao 
 	    
 	    if (isFunc && funcArgs.size()>0) {
-		System.out.println(spacing + "return: " + returnType);
-		System.out.println("__FuncArgs:__");
-		for (int i=0; i!=funcArgs.size(); i++) {
-		    funcArgs.get(i).printVar();
-		}
-		System.out.println("_____________");
+			
+	    	System.out.println(spacing + "Return: " + returnType + "\n");
+			System.out.println(spacing + "FuncArgs: ");
+			
+			for (int i=0; i!=funcArgs.size(); i++)
+			{
+				System.out.print(spacing + "\t");
+			    funcArgs.get(i).printVar();
+			}
+			
+			System.out.println();
 	    }
-	   
-
-
-	    // imprime variaveis locais
-	    System.out.println("--localVars:--");
-	    for (int i=0; i!=localVars.size(); i++) {
-		Variable var = localVars.get(i);
-		System.out.println("\n"+spacing); 
-		var.printVar();
-		 
-	    }  
-	    System.out.println("--------------");
+	    
 
 	    // print 'child' functions
-	    String space = spacing + "   ";
-	    for (int i=0; i!=childTables.size(); i++) {
-		childTables.get(i).printTable(space);
+	    
+	    System.out.println(spacing + "noChildTables: " + childTables.size() + "\n");
+	    String space = spacing + "\t";
+	    
+	    for (int i=0; i!=childTables.size(); i++)
+	    {
+	    	System.out.println(spacing + "ChildTable no. " + i + ":\n");
+	    	childTables.get(i).printTable(space);
 	    }
 	  
 	  }
+	  
+	  //Percorre as tabelas de simbolos e retorna a tabela com o nome e tipo indicados
+	  SymbolTable getSymbolTable(String stype, String sname)
+	  {  
+		  if(type.equals(stype) && name.equals(sname))
+			  return this;
+		  
+		  int childNum = this.childTables.size();
+			  
+		  if(childNum == 0)
+			  return null;
+		  
+		  for(int i = 0; i < childNum; i++)
+		  {
+			  SymbolTable s = childTables.get(i).getSymbolTable(stype, sname);
+			  
+			  if(s != null)
+				  return s;
+		  }
+		  
+		  return null;
+	  }
+	  
 	}
 
 
